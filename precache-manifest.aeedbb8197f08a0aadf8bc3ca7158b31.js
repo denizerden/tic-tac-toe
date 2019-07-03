08a3ac2a321355794fe53ef97c416503546a12a6
denizerden <denizerden@sabanciuniv.edu>
2019-07-03 14:47:00 +0300

    Updates

---
+++ precache-manifest.aeedbb8197f08a0aadf8bc3ca7158b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f1a54f9af038ba27fcd69b081f9dc9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ecc35563ce4144925e95",
+    "url": "/tic-tac-toe/static/css/main.3b465a60.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/tic-tac-toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "ecc35563ce4144925e95",
+    "url": "/tic-tac-toe/static/js/main.e227cd3a.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
